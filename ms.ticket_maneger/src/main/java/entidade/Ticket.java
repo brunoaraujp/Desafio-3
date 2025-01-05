768838c64bb3b26a495782879561d6bfd1a214e8
brunoaraujp <bg578889@gmail.com>
2025-01-05 15:09:30 -0300

    feature/criação da entidade tickt

---
+++ ms.ticket_maneger/src/main/java/entidade/Ticket.java
@@ -0,0 +1,19 @@
+package entidade;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "db_ticket")
+public class Ticket {
+
+    @Id
+    private int id;
+
+    private String Cpf;
+
+    private String quantidade ;
+
+
+
+}
