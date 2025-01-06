19973a145abf3058ba11ba445db074b1a0cac88a
brunoaraujp <bg578889@gmail.com>
2025-01-06 15:01:12 -0300

    featuere /"endpoin para atualizar e buscar por id"

--- ms.ticket_maneger/src/main/java/Controller/TicketControle.java
+++ ms.ticket_maneger/src/main/java/Controller/TicketControle.java
@@ -4,30 +4,45 @@ import entidade.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.TicketService;
 
 
 @RestController
-@RequestMapping(value = "api/Ticket")
+@RequestMapping( "api/Ticket")
 public class TicketControle {
 
 
-    @Autowired TicketService ticketService;
+    @Autowired
+    TicketService ticketService;
 
 
     @PostMapping
     public ResponseEntity<Ticket> criarticket(@RequestBody Ticket ticket) {
 
 
-         Ticket icket = ticketService.salvar(ticket);
+        Ticket icket = ticketService.salvar(ticket);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(icket);
 
 
     }
-}
 
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ticket> bucarcomid(@PathVariable String id) {
+
+        Ticket ticket = ticketService.buscarid(id);
+
+        return ResponseEntity.ok(ticket);
+
+
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Ticket>atualizartic (@PathVariable String id, @RequestBody Ticket ticket){
+        Ticket tic = ticketService.salvar(ticket);
+
+        return ResponseEntity.ok(tic);
+    }
+}
--- ms.ticket_maneger/src/main/java/service/TicketService.java
+++ ms.ticket_maneger/src/main/java/service/TicketService.java
@@ -2,8 +2,13 @@ package service;
 
 import entidade.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import repositorio.TicketRepositorio;
 
 @Service
@@ -13,9 +18,25 @@ public class TicketService {
     @Autowired
     TicketRepositorio ticketRepositorio;
 
+    public  Ticket atualizar(Ticket ticket) {
+        return ticketRepositorio.save(ticket);
+
+    }
+
+    public  Ticket buscarid(String id) {
+
+        return ticketRepositorio.findById(id).orElseThrow(
+                    ()-> new RuntimeException("id de Ticket não encontrado")
+            );
+        }
+
+
     @Transactional
     public Ticket salvar(Ticket ticket) {
         return ticketRepositorio.save(ticket);
 
     }
+
+
+
 }
